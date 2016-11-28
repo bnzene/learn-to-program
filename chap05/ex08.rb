6a9f3df8dad517913b489377c82bba4271c77a06
Eleanor Kavanagh-Brown <ekavanaghbrown@gmail.com>
2016-11-28 22:58:26 +0000

    Chapter 5 complete commit

---
+++ chap05/ex08.rb
@@ -0,0 +1,5 @@
+# angry boss program
+puts '*knocks on boss\'s door'
+puts 'Oh CHRIST, it\'s you - what the hell do you want? Can\'t you vultures just leave me alone for five minutes?! Oh GOD!'
+request = gets.chomp
+puts 'WHADDYA MEAN ' + '"' + request.to_s.upcase + '"' + '?! YOU\'RE FIRED! I NEVER WANNA COME ACROSS YOUR VILE FACE AGAIN, NOW GET OUT!'
---
+++ chap05/ex10.rb
@@ -0,0 +1,10 @@
+# More Arithmetic
+puts 5**2
+puts 5**0.5
+puts 7/3
+puts 7%3
+puts 7.8%2.67
+puts 365%7
+puts ''
+puts (5-2).abs
+puts (2-5).abs
