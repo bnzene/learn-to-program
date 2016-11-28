b64158f4f73d294813805d71032ce979ee59cef1
Eleanor Kavanagh-Brown <ekavanaghbrown@gmail.com>
2016-11-28 22:04:39 +0000

    Chap 05 exercises commit

---
+++ chap04/ex06.rb
@@ -0,0 +1,7 @@
+puts 'Hello there, this is Eddie, your shipboard computer and I am just thrilled to have you on board! In order that I can begin to establish an easygoing rapport with you as is conducive to pleasant intergalactic travel, please will you tell me your first name?'
+firstname = gets.chomp
+puts 'Thanks, ' + firstname + '! What\'s your middle name?'
+midname = gets.chomp
+puts 'What a great name - and how about your surname?'
+surname = gets.chomp
+puts 'What a wonderful name, ' + firstname + ' ' + midname + ' ' + surname + '! Thank you so much! We\'re gonna have a lot of fun together, I just know it!'
---
+++ chap04/ex07.rb
@@ -0,0 +1,3 @@
+puts 'What\'s your favourite number?'
+number = gets.chomp
+puts 'Have you thought about trying ' + (number.to_i + 1).to_s + ' on for size? It\'s bigger AND better!'
